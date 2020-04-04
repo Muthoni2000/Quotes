deaa7b6365dcccc689050225e3d8b11849effa09
Muthoni2000 <bmuthoni39@yahoo.com>
2020-04-04 21:44:01 +0300

    added changes on quote.ts

---
+++ src/app/quote.ts
@@ -0,0 +1,5 @@
+export class Quote {
+    constructor(public id:number,public yourname:string,public theQuote:string, public quoteauthor:string, public postedDate: Date, public upvotes:number, public downvotes:number){}
+  }
+
+  
